5667195a57a8a1f0e171c783c4e4d8fc4b55ffc4
wahab <aderintoadetunji@gmail.com>
2021-05-20 08:03:07 +0100

    header markup and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-       <h1>Lets get Started</h1>
+       <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import '../styles/header.css'
+import MenuIcon from '@material-ui/icons/Menu'
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import AppsIcon from '@material-ui/icons/Apps';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import Avatar from '@material-ui/core/Avatar';
 
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
+        <div className= "header">
+            <div className="header__left">
+                <MenuIcon />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt = "logo" className="header__logo" />
+            </div>
+            <div className="header__center">
+                <div className="header__input">
+                    <input type="text" />
+                    <button>
+                        <SearchIcon className="header__search"/>
+                    </button>
+                </div>
+                <MicIcon />
+            </div>
+            <div className="header__right">
+                <VideoCallIcon className="header__icons"  className="header__icons"/>
+                <AppsIcon  className="header__icons"/>
+                <NotificationsIcon  className="header__icons"/>
+                <Avatar className="header__icons">W</Avatar>
+            </div>
         </div>
     )
 }
